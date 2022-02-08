ebafeb28945140b495e11d4de45f2d527f0f67c5
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2022-02-08 14:34:07 +0600

    Fix a small bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# For readthedocs
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
