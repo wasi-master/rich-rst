4df27714df09e993997ec510fbfa6330f193c8d6
Wasi Master <arianmollik323@gmail.com>
2024-01-27 01:04:49 +0600

    Bump version to 1.2.0

--- rich_rst/__init__.py
+++ rich_rst/__init__.py
@@ -35,7 +35,7 @@ from pygments.util import ClassNotFound
 
 __all__ = ("RST", "ReStructuredText", "reStructuredText", "RestructuredText")
 __author__ = "Arian Mollik Wasi (aka. Wasi Master)"
-__version__ = "1.1.7"
+__version__ = "1.2.0"
 
 install()
 
